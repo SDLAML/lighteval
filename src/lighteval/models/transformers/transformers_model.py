a51e008948103760842ee27d479ac7302e5a338a
Oleg Filatov <20295221+ofivite@users.noreply.github.com>
2026-01-24 20:18:08 +0100

    handle empty stop_sequences in TransformersModel

--- src/lighteval/models/transformers/transformers_model.py
+++ src/lighteval/models/transformers/transformers_model.py
@@ -702,7 +702,7 @@ class TransformersModel(LightevalModel):
                     # NOTE: we are assuming all items in a batch behave similarly (same
                     # stop_tokens and max_tokens genrated) which is not necessarily
                     # the case! Because of that we only use batch size of 1
-                    stop_tokens = [self.tokenizer.eos_token] + batch[0].stop_sequences
+                    stop_tokens = [self.tokenizer.eos_token] + batch[0].stop_sequences if len(batch[0].stop_sequences) > 0 else [self.tokenizer.eos_token]
 
                 max_new_tokens = batch[0].generation_size
                 num_samples = batch[0].num_samples
