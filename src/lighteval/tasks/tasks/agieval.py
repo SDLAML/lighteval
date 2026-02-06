58d64ae2d61767829103a5f6303f4462319231ca
Oleg Filatov <20295221+ofivite@users.noreply.github.com>
2026-02-06 13:38:55 +0100

    Add CoT EM eng-only agieval + refactoring

--- src/lighteval/tasks/tasks/agieval.py
+++ src/lighteval/tasks/tasks/agieval.py
@@ -45,370 +45,93 @@ def record_to_sample(record):
 
 
 def agieval_prompt(line, task_name: str = None):
+    if line["gold"][0] >= len(line["choices"]):
+        print("Warning: gold index is out of bounds for choices, skipping sample.")
+        return None
     return Doc(
         task_name=task_name,
-        query=line["query"],
+        query=line["query"].strip(),
         choices=[f" {c}" for c in line["choices"]],
         gold_index=line["gold"],
     )
 
+def agieval_prompt_cot_eng(line, task_name: str = None):
+    query = f"Answer the following multiple choice question (with answers). Think step by step before giving the final answer.\n\nQuestion: {line['question']}"
+    query += "".join([f"\n{key}. {choice.replace("(A)", "").replace("(B)", "").replace("(C)", "").replace("(D)", "").replace("(E)", "").strip()}" 
+                      for key, choice in zip(ascii_uppercase, line["options"])])
+    query += "\nSolution:"
 
-agieval_aqua_rat = LightevalTaskConfig(
-    name="agieval:aqua-rat",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-aqua-rat",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=[f" {c}" for c in ascii_uppercase[:len(line["options"])]],
+        gold_index=ascii_uppercase.index(line["label"]),
+    )
 
-agieval_gaokao_biology = LightevalTaskConfig(
-    name="agieval:gaokao-biology",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-gaokao-biology",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_gaokao_chemistry = LightevalTaskConfig(
-    name="agieval:gaokao-chemistry",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-gaokao-chemistry",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_gaokao_chinese = LightevalTaskConfig(
-    name="agieval:gaokao-chinese",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-gaokao-chinese",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_gaokao_english = LightevalTaskConfig(
-    name="agieval:gaokao-english",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-gaokao-english",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_gaokao_geography = LightevalTaskConfig(
-    name="agieval:gaokao-geography",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-gaokao-geography",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_gaokao_history = LightevalTaskConfig(
-    name="agieval:gaokao-history",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-gaokao-history",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_gaokao_mathqa = LightevalTaskConfig(
-    name="agieval:gaokao-mathqa",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-gaokao-mathqa",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_gaokao_physics = LightevalTaskConfig(
-    name="agieval:gaokao-physics",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-gaokao-physics",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_logiqa_en = LightevalTaskConfig(
-    name="agieval:logiqa-en",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-logiqa-en",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_logiqa_zh = LightevalTaskConfig(
-    name="agieval:logiqa-zh",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-logiqa-zh",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_lsat_ar = LightevalTaskConfig(
-    name="agieval:lsat-ar",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-lsat-ar",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_lsat_lr = LightevalTaskConfig(
-    name="agieval:lsat-lr",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-lsat-lr",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_lsat_rc = LightevalTaskConfig(
-    name="agieval:lsat-rc",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-lsat-rc",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_sat_en = LightevalTaskConfig(
-    name="agieval:sat-en",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-sat-en",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_sat_en_without_passage = LightevalTaskConfig(
-    name="agieval:sat-en-without-passage",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-sat-en-without-passage",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
-
-agieval_sat_math = LightevalTaskConfig(
-    name="agieval:sat-math",
-    sample_fields=record_to_sample,
-    solver=[multiple_choice(cache=True)],
-    scorer=choice(),
-    prompt_function=agieval_prompt,
-    hf_repo="dmayhem93/agieval-sat-math",
-    hf_subset="default",
-    hf_avail_splits=["test"],
-    evaluation_splits=["test"],
-    few_shots_split=None,
-    few_shots_select="random_sampling",
-    generation_size=1,
-    metrics=[
-        Metrics.loglikelihood_acc,
-    ],
-    stop_sequence=None,
-    version=0,
-)
+AGIEVAL_SUBSETS = [
+    "aqua_rat",
+    # "aqua-rat",
+    # "gaokao-biology",
+    # "gaokao-chemistry",
+    # "gaokao-chinese",
+    # "gaokao-english",
+    # "gaokao-geography",
+    # "gaokao-history",
+    # "gaokao-mathqa",
+    # "gaokao-physics",
+    "logiqa-en",
+    # "logiqa-zh",
+    "lsat-ar",
+    "lsat-lr",
+    "lsat-rc",
+    "sat-en",
+    # "sat-en-without-passage",
+    "sat-math",
+]
 
 TASKS_TABLE = [
-    agieval_aqua_rat,
-    agieval_gaokao_biology,
-    agieval_gaokao_chemistry,
-    agieval_gaokao_chinese,
-    agieval_gaokao_english,
-    agieval_gaokao_geography,
-    agieval_gaokao_history,
-    agieval_gaokao_mathqa,
-    agieval_gaokao_physics,
-    agieval_logiqa_en,
-    agieval_logiqa_zh,
-    agieval_lsat_ar,
-    agieval_lsat_lr,
-    agieval_lsat_rc,
-    agieval_sat_en,
-    agieval_sat_en_without_passage,
-    agieval_sat_math,
+    LightevalTaskConfig(
+        name=f"agieval_eng_em:{subset}",
+        sample_fields=record_to_sample,
+        solver=[multiple_choice(cache=True)],
+        scorer=choice(),
+        prompt_function=agieval_prompt_cot_eng,
+        hf_repo="lighteval/agi_eval_en",
+        hf_subset=subset,
+        hf_avail_splits=["train", "validation"],
+        evaluation_splits=["train"],
+        few_shots_split="validation",
+        few_shots_select=None,
+        generation_size=512,
+        metrics=[
+            # Metrics.exact_match,
+            Metrics.gpqa_instruct_metric,
+        ],
+        stop_sequence=["Question:"],
+        version=0,
+    )
+    for subset in AGIEVAL_SUBSETS
 ]
+
+# TASKS_TABLE = [
+#     LightevalTaskConfig(
+#         name=f"agieval:{subset}",
+#         sample_fields=record_to_sample,
+#         solver=[multiple_choice(cache=True)],
+#         scorer=choice(),
+#         prompt_function=agieval_prompt,
+#         hf_repo=f"dmayhem93/agieval-{subset}",
+#         hf_subset="default",
+#         hf_avail_splits=["test"],
+#         evaluation_splits=["test"],
+#         few_shots_split=None,
+#         few_shots_select="random_sampling",
+#         generation_size=1,
+#         metrics=[
+#             Metrics.loglikelihood_acc,
+#         ],
+#         stop_sequence=None,
+#         version=0,
+#     )
+#     for subset in AGIEVAL_SUBSETS
+# ]
