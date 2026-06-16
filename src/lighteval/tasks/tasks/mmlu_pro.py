311fc19b9e83cdea9dda2a71581c96a7574a1768
Oleg Filatov <20295221+ofivite@users.noreply.github.com>
2026-06-16 17:26:54 +0200

    increase generation size to 32768 in Mmlu and MMLU Pro chat regimes

--- src/lighteval/tasks/tasks/mmlu_pro.py
+++ src/lighteval/tasks/tasks/mmlu_pro.py
@@ -110,6 +110,7 @@ mmlu_pro_cot = LightevalTaskConfig(
     hf_revision="3373e0b32277875b8db2aa555a333b78a08477ea",
     evaluation_splits=("test",),
     few_shots_split="validation",
+    generation_size=32768,
     metrics=[Metrics.gpqa_instruct_metric],
 )
 
